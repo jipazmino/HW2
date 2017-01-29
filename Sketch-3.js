7837d79d1ae7d5eb9a125a9fa9ba29c05da24d72
jipazmino <jpazminomino@cca.edu>
2017-01-29 00:00:58 -0800

    Create Sketch-3.js

---
+++ Sketch-3.js
@@ -0,0 +1,19 @@
+3.
+
+
+	var a = 100
+  var b = 100
+
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+   
+	background (255);
+  ellipse(a,b,50,50);
+  
+  a=a + 3
+}
+
